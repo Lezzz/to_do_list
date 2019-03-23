1856e81efe0be0ba1aa76de115d7c0e020298345
Lezzz <valentin.lazureanu@gmail.com>
2019-03-23 20:53:41 +0200

    FINAL PRODUCT

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -21,6 +21,12 @@ $("input[type = 'text']").on("keypress", function(event) {
     var toDoText = $(this).val();
     $(this).val("");
     //create new li, add to ul
-    $("ul").append("<li><span>X</span> " + toDoText + "</li>");
+    $("ul").append(
+      "<li><span><i class='far fa-trash-alt'></i></span> " + toDoText + "</li>"
+    );
   }
+});
+
+$(".fa-eye-slash").on("click", function() {
+  $("input[type='text'").fadeToggle();
 });
