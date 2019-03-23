191e3f985d84f73894db401882376fb68a3bdecb
Lezzz <valentin.lazureanu@gmail.com>
2019-03-23 19:39:35 +0200

    Added functionality to add new items to to do list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,9 +3,23 @@ $("li").click(function() {
     $(this).toggleClass("completed");
 });
 
+//delete functionality and animation
 $("span").click(function(event){
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
+    // stops bubbling up
     event.stopPropagation();
+});
+
+//creating todos
+$("input[type = 'text']").keypress(function(event){
+    if(event.which === 13){
+        //grab text
+       var toDoText = $(this).val();
+       //create new li, add to ul
+       $("ul").append("<li>" + toDoText+ "</li>");
+       toDoText = "";
+       $("input").val("");
+    }
 });
